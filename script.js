0bda5318e68a1ba82661ad1cf9d9125355d97b0e
Ma. Chey Cheneb Bico <mccbico.dev@gmail.com>
2026-07-07 00:30:33 +0800

    MFA

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ projects.forEach(project => {
     <p class="tech">Developed with ${project.tech.map(t => `<span>${t}</span>`).join(", ")}</p>
     <p class="desc">${project.desc}</p>
     <div class="project-buttons">
-      <a href="${project.demoLink}" target="_blank" class="btn primary">Live Demo <i class="fa-solid fa-arrow-right"></i></a>
+      <a href="${project.demoLink}" target="_blank" class="btn primary">Demo <i class="fa-solid fa-arrow-right"></i></a>
       <a href="${project.githubLink}" target="_blank" class="btn secondary"><i class="fab fa-github"></i> GitHub</a>
     </div>
   `;
